93284405b8807e42a9b18dff1fc83c41557fef43
Nero Solfer <sanetto616@gmail.com>
2022-06-20 21:38:52 +0500

    Revert "авторизация готова ура"
    This reverts commit ecfabec078ec5ef2028a7c7d2676703b7774da72.

--- фяцычувксаепмир/Form1.Designer.cs
+++ фяцычувксаепмир/Form1.Designer.cs
@@ -32,7 +32,6 @@ namespace фяцычувксаепмир
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,24 +46,13 @@ namespace фяцычувксаепмир
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(78, 46);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(401, 29);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "тест кнопка для открытия формы 2";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -81,7 +69,6 @@ namespace фяцычувксаепмир
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private Button button1;
     }
 }
 
--- фяцычувксаепмир/Program.cs
+++ фяцычувксаепмир/Program.cs
@@ -16,7 +16,7 @@ namespace фяцычувксаепмир
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new Form1());
         }
     }
 }
